d5639476a6fba58ba0101a308959c0eefded5981
mdi80 <76841566+mdi80@users.noreply.github.com>
2022-03-22 23:21:26 +0430

    Update settings.py

--- karma/settings.py
+++ karma/settings.py
@@ -8,8 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yp&t!xxn)p%5s1)wfs72l#4(ddidnmd##ozszc^$dj&mgfqx^f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
